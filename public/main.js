cd25fee591763f27bceaef44409238a112b43430
khw0123 <khw18837@gmail.com>
2025-03-14 14:01:25 +0900

    main.js로 js 이동

---
+++ public/main.js
@@ -0,0 +1,7 @@
+const watchBtn = document.querySelector(".word-container .w1");
+const editBtn = document.querySelector(".word-container .w2");
+const lunchBtn = document.querySelector(".word-container .w3");
+
+watchBtn.addEventListener("click", function () {
+  window.location.href = "time.ejs";
+});
